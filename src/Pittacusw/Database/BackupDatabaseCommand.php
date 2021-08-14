dae7f6926005e5caa9864b4f711152b392572f0b
pittacusw <luke707@gmail.com>
2021-08-13 22:29:28 -0400

    composer update

--- src/Pittacusw/Database/BackupDatabaseCommand.php
+++ src/Pittacusw/Database/BackupDatabaseCommand.php
@@ -39,6 +39,9 @@ class BackupDatabaseCommand extends Command {
   $user = env('DB_USERNAME');
   $db   = env('DB_DATABASE');
   $pass = env('DB_PASSWORD');
+  if (!is_dir('database/sql')) {
+   mkdir('database/sql');
+  }
   collect(DB::select('SHOW TABLES'))->each(function($table) use ($db, $user, $host, $pass) {
    $name    = 'Tables_in_' . $db;
    $file    = "database/sql/" . $table->$name . ".sql.gz";
